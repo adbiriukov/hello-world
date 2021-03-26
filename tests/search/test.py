9b29957a08a0f7afeba2dcb200d604c8f085f3d0
adbiriukov <adbiriukov@gmail.com>
2021-03-26 22:21:18 +0500

    add several tests to az_search

--- tests/search/test.py
+++ tests/search/test.py
@@ -3,6 +3,11 @@ import allure
 import pytest
 import time
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 driver = webdriver.Chrome(executable_path="../../chromedriver.exe")
 driver.implicitly_wait(5)
 driver.maximize_window()
@@ -12,10 +17,11 @@ driver.get('https://www.amazon.com/')
 
 search_field = driver.find_element_by_name('field-keywords')
 search_field.clear()
-search_field.send_keys('google')
+search_field.send_keys('phone')
 search_field.submit()
 
-a = driver.find_element_by_xpath('//*[@id="search"]/span/div/span/h1/div/div[1]/div/div/span[1]')
-assert '1-48' in a.text
+for x in range(1, 99):
+    print(x)
+    print(driver.find_element_by_xpath("//*[@id='search']/div[1]/div/div[1]/div/span[3]/div[2]/div["+str(x)+"]").is_displayed())
 
 driver.quit()
